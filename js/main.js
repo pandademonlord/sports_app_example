e15ee02b8ded75b441207b08ce3b64f6619d0c34
Michael Knoche <mknoche@gmail.com>
2016-05-08 09:42:25 -0700

    inital commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="angular.js" />
+// Your code here!
+angular.module("sportsStore")
+    .constant("dataUrl","http://localhost:5500/products")
+    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+        $scope.data = {};
+
+        $http.get(dataUrl)
+        .success(function (data) {
+            $scope.data.products = data;
+        })
+        .error(function (error) {
+            $scope.data.error = error;
+        });
+})
